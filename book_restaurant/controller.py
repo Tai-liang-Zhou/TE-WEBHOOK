c22355c1a6d0d82e3c84363da5a77853591686bd
Tom <ga731852@gmail>
2019-03-15 10:21:36 +0000

    new version v8 2019/3/15

--- book_restaurant/controller.py
+++ book_restaurant/controller.py
@@ -4,6 +4,7 @@ from flask import request
 from flask_restful import Resource
 from dateutil.parser import parse
 from langconv import Converter
+from datetime import datetime
 import json
 import re
 import requests
@@ -53,14 +54,14 @@ def get_num(num_str):
     payload = {
         "id": "integer-number",
         "hasContext": False,
-        "query": num_str
+        "query": Converter('zh-hans').convert(num_str)
     }
     payload = json.dumps(payload, ensure_ascii=False)
     payload = payload.encode('utf-8')
     headers = {'content-type': 'application/json'}
     r = requests.post(constants.TDE_URL, payload, timeout=float(constants.REQUEST_TIMEOUT), headers=headers)
     r_obj = r.json()
-    LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
+    # LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
     try :
         num = r_obj['informs'][0]['value']['displayText']
     except :
@@ -77,14 +78,14 @@ def get_phone_num(num_str):
                 "regions" : ["mainland","tw"],
                 "types":["mobile","landline"]
                },
-        "query": num_str
+        "query": Converter('zh-hans').convert(num_str)
     }
     payload = json.dumps(payload, ensure_ascii=False)
     payload = payload.encode('utf-8')
     headers = {'content-type': 'application/json'}
     r = requests.post(constants.TDE_URL, payload, timeout=float(constants.REQUEST_TIMEOUT), headers=headers)
     r_obj = r.json()
-    LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
+    # LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
     try:
         phone = r_obj['informs'][0]['value']['displayText']
     except:
@@ -108,23 +109,28 @@ def get_hourtime(hour_str):
             "onlyOne": "last"
         }
     },
-    "query": hour_str,
+    "query": Converter('zh-hans').convert(hour_str),
     }
     payload = json.dumps(payload, ensure_ascii=False)
     payload = payload.encode('utf-8')
     headers = {'content-type': 'application/json'}
     r = requests.post(constants.TDE_URL, payload, timeout=float(constants.REQUEST_TIMEOUT), headers=headers)
-    error_msg = 'API invocation fail,url:%s, status_code:%s, response:%s'\
-                % (constants.TDE_URL, r.status_code, r.text)
-    LOG.error(error_msg)
+    # error_msg = 'API invocation fail,url:%s, status_code:%s, response:%s'\
+    #             % (constants.TDE_URL, r.status_code, r.text)
+    # LOG.error(error_msg)
     r_obj = r.json()
-    LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
+
+    
+    # LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
+    minute = 'minute_enable'
+    if 'minute' not in r_obj['informs'][0]['value']['chrono']['time']['items'][0]:
+        minute = 'minute_disable'
     try:
         hour = r_obj['informs'][0]['value']['chrono']['time']['items'][0]['hour']
     except:
         hour = None
         LOG.error('type error form exact_hour')
-    return hour
+    return hour, minute
     
 def get_datetime(datetime_str):
     # get the turely data time ex : yyyy-mm-dd hh:mm:ss 
@@ -142,21 +148,22 @@ def get_datetime(datetime_str):
                 "onlyOne": "last"
             }
         },
-        "query": datetime_str,
+        "query": Converter('zh-hans').convert(datetime_str),
     }
     payload = json.dumps(payload, ensure_ascii=False)
     payload = payload.encode('utf-8')
     headers = {'content-type': 'application/json'}
     r = requests.post(constants.TDE_URL, payload, timeout=float(constants.REQUEST_TIMEOUT), headers=headers)
-    error_msg = 'API invocation fail,url:%s, status_code:%s, response:%s'\
-                % (constants.TDE_URL, r.status_code, r.text)
-    LOG.error(error_msg)
+    # error_msg = 'API invocation fail,url:%s, status_code:%s, response:%s'\
+    #             % (constants.TDE_URL, r.status_code, r.text)
+    # LOG.error(error_msg)
     r_obj = r.json()
-    LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
+    # LOG.info(json.dumps(r_obj, ensure_ascii=False, indent=4))
     try:
         dt = parse(r_obj['informs'][0]['value']['chrono']['time']['items'][0]['ISO_DATE']['single'])
     except :
-        dt = "0000-00-00"
+        dt = "0001-01-01"
+        dt = datetime.strptime(dt, '%Y-%m-%d')
         LOG.error('type error form exact_date')
     return dt
 
@@ -194,7 +201,7 @@ class InlineStartBooking(Resource):
 
 class InlineDoBooking(Resource):
     def post(self):
-        json_from_request = json.loads(Converter('zh-hant').convert(request.stream.read().decode('utf-8')))
+        json_from_request = json.loads(Converter('zh-hans').convert(request.stream.read().decode('utf-8')))
         task_info = json_from_request['task_info']
         te_payload = {
             "user_id": json_from_request['user_id'],
@@ -205,13 +212,13 @@ class InlineDoBooking(Resource):
                 "restaurant_id": task_info.get('restaurant_id', None),
                 "seat_num": task_info.get('seat_num', None),
                 "kid_num": task_info.get('seat_num_children', None),
-                "chair_num": task_info.get('seat_num_children', None),
+                "chair_num": task_info.get('chair_num', None) ,# if "chair_num" in task_info else 0
                 "lastname": task_info.get('lastname', None),
                 "phone": task_info.get('phone', None),
                 "note": task_info.get('note', None),
             }
         }
-        # LOG.debug('data received from TE: %s' % json.dumps(te_payload, ensure_ascii=False, indent=4))
+        LOG.debug('In doBooking, data received from TE: %s' % json.dumps(te_payload, ensure_ascii=False, indent=4))
 
         task_info = te_payload['task_info']
         payload = {
@@ -222,8 +229,8 @@ class InlineDoBooking(Resource):
                 "time": task_info['time_time'] if task_info['time_time'] is not None else '',
                 "restaurant_id": task_info['restaurant_id'] if task_info['restaurant_id'] is not None else '',
                 "group_num": int(task_info['seat_num']) if task_info['seat_num'] is not None else 0,
-                "kid_num": int(task_info['kid_num']) if task_info['kid_num'] is not None else 0,
-                "chair_num": int(task_info['chair_num']) if task_info['chair_num'] is not None else 0,
+                "kid_num": int(task_info['kid_num']) if task_info['kid_num'] is not None and task_info['kid_num'] != "null" else 0,
+                "chair_num": int(get_num(task_info['chair_num'])) if task_info['chair_num'] is not None else 0,
                 "lastname": task_info['lastname'] if task_info['lastname'] is not None else '',
                 "phone": get_phone_num(task_info['phone']) if task_info['phone'] is not None else '',
                 "note": task_info['note'] if task_info['note'] is not None else '',
@@ -240,8 +247,9 @@ class InlineDoBooking(Resource):
         r_obj = r.json()
         LOG.debug('response: %s' % json.dumps(r_obj, ensure_ascii=False, indent=4))
         update_kv_map = r_obj["msg_response"]["update"]
-        # Assign 0 to chair_num if user doesn't want to baby chair
-        update_kv_map['chair_num'] = int(task_info['chair_num']) if task_info['chair_num'] is not None else 0
+        # Assign 0 to chair_num if user doesn't want any baby chair
+        if 'chair_num' not in json_from_request['task_info']:
+            update_kv_map['chair_num'] = '0' 
         ret = encapsule_rtn_format(update_kv_map, None)
         return Response(json.dumps(ret), status=200)
 
@@ -451,10 +459,9 @@ class ConvertParamsNLU2(Resource):
         json_from_request = json.loads(Converter('zh-hans').convert(request.stream.read().decode('utf-8')))
 
         # print the format of json
-        LOG.debug('data received from TE: %s' % json.dumps(json_from_request['task_info'], ensure_ascii=False, indent=4))
+        LOG.debug('In ConvertParamsNLU2, data received from TE: %s' % json.dumps(json_from_request['task_info'], ensure_ascii=False, indent=4))
 
         # parser time
-        time = json_from_request['task_info']['time']
         exact_date = json_from_request['task_info']['exact_date']
         exact_hour = json_from_request['task_info']['exact_hour']
         exact_minute = json_from_request['task_info']['exact_minute']
@@ -483,7 +490,7 @@ class ConvertParams(Resource):
         json_from_request = json.loads(Converter('zh-hans').convert(request.stream.read().decode('utf-8')))
 
         # print the format of json
-        LOG.debug('data received from TE: %s' % json.dumps(json_from_request['task_info'], ensure_ascii=False, indent=4))
+        LOG.debug('In ConvertParams, data received from TE: %s' % json.dumps(json_from_request['task_info'], ensure_ascii=False, indent=4))
 
         # parse seat number
         seat_num = get_num(json_from_request['task_info']['seat_num'])
@@ -491,7 +498,7 @@ class ConvertParams(Resource):
             
         # parse children seat number and chair number
         seat_num_children = 0
-        if 'seat_num_children' in json_from_request['task_info']:
+        if 'seat_num_children' in json_from_request['task_info'] and json_from_request['task_info']['seat_num_children'] != "null":
             seat_num_children = get_num(json_from_request['task_info']['seat_num_children'])
             seat_num_total = int(seat_num_total) + int(seat_num_children)
         # parser date
@@ -507,10 +514,29 @@ class ConvertParams(Resource):
 
         # parse exact date
         dt = get_datetime(exact_date)
-        exact_hour = get_hourtime(exact_hour)
-        exact_minute = int(get_num(exact_minute))
 
-        LOG.info('hour:'+str(exact_hour)+' minute:'+str(exact_minute))
+        time_judge = re.compile(u'(早|晚|凌晨|上午|中午|下午|晚上)').search(exact_hour)
+        if time_judge is not None:
+            time_judge = time_judge.group(1)
+
+        exact_hour, minute = get_hourtime(exact_hour)
+        if minute == 'minute_disable':
+            exact_minute = '0'
+
+        # opening hours from 9am to 10pm 
+
+        if time_judge in [u'晚',u'中午',u'下午',u'晚上']:
+            if exact_hour < 12:
+                exact_hour += 12
+
+        if exact_hour < 10:
+            exact_hour += 12
+        elif exact_hour > 22:
+            exact_hour-=12
+
+        exact_minute = int(get_num(str(exact_minute)))
+
+        # LOG.info('hour:'+str(exact_hour)+' minute:'+str(exact_minute))
 
         dt = dt.replace(hour=exact_hour, minute=exact_minute)
 
@@ -521,7 +547,8 @@ class ConvertParams(Resource):
             "seat_num": seat_num,
             "seat_num_total": seat_num_total
         }
-
+        
+        # date format not include minute if minute more than zero
         if exact_minute > 0 :
             update_kv_map['time_str'] = dt.strftime("%m{M}%d{d}%H{h}%M{m}").format(M='月', d='日', h='點', m='分')
         else :
@@ -529,6 +556,7 @@ class ConvertParams(Resource):
 
         if seat_num_children != 0:
             update_kv_map['seat_num_children'] = seat_num_children
+
 
         ret = encapsule_rtn_format(update_kv_map, None)
         return Response(json.dumps(ret), status=200)
@@ -621,7 +649,7 @@ class ResetParams(Resource):
         json_from_request = json.loads(Converter('zh-hant').convert(request.stream.read().decode('utf-8')))
 
         # print the format of json
-        LOG.debug('data received from TE: %s' % json.dumps(json_from_request, ensure_ascii=False, indent=4))
+        LOG.debug('In ResetParams, data received from TE: %s' % json.dumps(json_from_request, ensure_ascii=False, indent=4))
 
         task_info = json_from_request['task_info']
         LOG.info(json.dumps(task_info, ensure_ascii=False))
@@ -634,9 +662,7 @@ class ResetParams(Resource):
             remove_kv_map["lastname"] = None
         if 'time_str' not in task_info:
             remove_kv_map["exact_date"] = None
-            remove_kv_map["exact_date_confirm"] = None
             remove_kv_map["exact_hour"] = None
-            remove_kv_map["exact_hour_confirm"] = None
         ret = encapsule_rtn_format(None, remove_kv_map)
         return Response(json.dumps(ret), status=200)
         
--- constants.py
+++ constants.py
@@ -6,4 +6,4 @@ import os
 SYSTEX_URL = os.environ.get('TW_SYSTEX_URL', 'http://vbooking.eastasia.cloudapp.azure.com:8080/VBooking/rest/')
 COMMON_PARSER_SERVICE_URL = os.environ.get('TW_COMMON_PARSER_SERVICE_URL', 'http://dev1.emotibot.com:14901/common-parser-service')
 TDE_URL = os.environ.get('TDE_URL', 'http://poc2.emotibot.com:10999/tde/usp/parse')
-REQUEST_TIMEOUT = os.environ.get('TW_REQUEST_TIMEOUT', '4')
+REQUEST_TIMEOUT = os.environ.get('TW_REQUEST_TIMEOUT', '10')
